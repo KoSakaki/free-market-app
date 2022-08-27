d955f7b8f4b99a35710aa3d05579da744898adb1
KoSakaki <sakaki@flaginc.net>
2022-08-27 17:01:15 +0900

    makee create Method

--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -1,8 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Item } from './item.model';
 
 @Injectable()
 export class ItemsService {
+  private items: Item[] = [];
   findAll() {
     return "this is items Service";
   }
+
+
+  create(item: Item): Item {
+    this.items.push(item)
+    return item
+  }
+
+  
 }
