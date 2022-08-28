b0381b383e1e39b06d80a26677aa0cb209c0e94d
KoSakaki <sakaki@flaginc.net>
2022-08-28 11:13:21 +0900

    create: validation

--- src/items/items.controller.ts
+++ src/items/items.controller.ts
@@ -1,34 +1,42 @@
-import { Body, Controller, Delete, Get,Param, Patch, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { ItemsService } from './items.service';
 import { Item } from './item.model';
 import { CreateItemDto } from './dto/creata-item.dto';
 
 @Controller('items')
 export class ItemsController {
-
-  constructor(private readonly ItemsService: ItemsService){}
+  constructor(private readonly ItemsService: ItemsService) {}
   @Get()
-  findAll():Item[] {
+  findAll(): Item[] {
     return this.ItemsService.findAll();
   }
 
   @Get(':id') // items/:id
-  findById(@Param('id') id: string): Item {
+  findById(@Param('id', ParseUUIDPipe) id: string): Item {
     return this.ItemsService.findById(id);
   }
 
   @Post()
-  create(@Body() createItemDto: CreateItemDto ): Item {
+  create(@Body() createItemDto: CreateItemDto): Item {
     return this.ItemsService.create(createItemDto);
   }
 
   @Patch(':id')
-  updateStatus(@Param('id') id: string): Item {
+  updateStatus(@Param('id', ParseUUIDPipe) id: string): Item {
     return this.ItemsService.updateStatus(id);
   }
 
   @Delete(':id')
-  delete(@Param('id') id: string): void {
+  delete(@Param('id', ParseUUIDPipe) id: string): void {
     return this.ItemsService.delete(id);
   }
 }
--- src/items/items.service.ts
+++ src/items/items.service.ts
@@ -2,34 +2,36 @@ import { Injectable } from '@nestjs/common';
 import { CreateItemDto } from './dto/creata-item.dto';
 import { ItemStatus } from './item-status.enum';
 import { Item } from './item.model';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class ItemsService {
   private items: Item[] = [];
-  findAll():Item[] {
+  findAll(): Item[] {
     return this.items;
   }
 
   findById(id: string): Item {
-    return this.items.find(item=>item.id===id)
+    return this.items.find((item) => item.id === id);
   }
 
   create(createItemDto: CreateItemDto): Item {
     const item = {
+      id: uuid(),
       ...createItemDto,
       status: ItemStatus.ON_SALE,
-    }
-    this.items.push(item)
-    return item
+    };
+    this.items.push(item);
+    return item;
   }
 
-  updateStatus(id: string) : Item {
+  updateStatus(id: string): Item {
     const item = this.findById(id);
     item.status = ItemStatus.SOLD_OUT;
     return item;
   }
 
   delete(id: string): void {
-    this.items = this.items.filter(item => item.id !== id);
+    this.items = this.items.filter((item) => item.id !== id);
   }
 }
